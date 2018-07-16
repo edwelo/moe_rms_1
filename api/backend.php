0eae5137afc96996b123724549c396a187db5c99
Edwel Ongrung <edwel@palaumoe.net>
2018-07-16 22:16:02 +0900

    save v1

--- api/backend.php
+++ api/backend.php
@@ -16,96 +16,160 @@ $rqItemsTable = "finance3.requisitions_items";
 $vendorTable = "finance3.vendors";
 $staffTable = "personnel.tblbiodata";
 
+if($_GET["a"]) {
+	$tmp["fld1"] = "fld1";
+	$tmp["fld2"] = "fld2";
+	echo "
+<form action='' method=post>
+	Use this to test post:
+	<input type=text name=RequisitionId value=5 />&nbsp;&nbsp;
+	<input type=text name=Data value='" . json_encode($tmp) . "' />&nbsp;&nbsp;
+	<input type=submit value=post />
+</form>
+<br />
+<br />
+	";
+}
+
+unset($rqId);
+if($_GET["requisitionId"] == "new" || is_numeric($_GET["requisitionId"])) $rqId = $_GET["requisitionId"];
+
 if(isset($_GET["save"])) {
-	$save_arr = json_decode(trim($_GET["save"]), true);
-	
+
 	if($_GET["a"]) {
-		echo "json from \$_GET[\"save\"]:<br /><br />\n" . $_GET["save"] . "<br /><br />\n
-		json_decode(\$_GET[\"save\"]):<br />\n";
+		echo "json from \$_GET[\"save\"]:<br /><br />\n" . $_GET["save"] . "<br /><br />\n";
+	}
+
+	if(isset($_POST)) {
+		$save_arr = json_decode(file_get_contents('php://input'));
+		#echo "post_got_this_" . json_encode($save_arr);
+		#echo "<pre>"; print_r($save_arr); echo "</pre>"; exit;
+	} else {
+		echo "POST_NOT_RECEIVED!";
+		exit;
+	}
+
+	if($_GET["a"]) {
+		echo "array from json:<br /><br />\n";
 		echo "<pre>"; print_r($save_arr); echo "</pre>";
 	}
 
 	//build rq sql
-	
-	$rqId = $save_arr["requisitionId"];
-	unset($save_arr["requisitionId"]);
-	$items = $save_arr["items"];
-	unset($save_arr["items"]);
-	
+
+	$rqId = $save_arr->requisitionId;
+	unset($save_arr->requisitionId);
+	$items = $save_arr->items;
+	unset($save_arr->items);
+
 	$sql = "";
-	foreach($save_arr as $key=>$value) { 
-		$value = mysqli_real_escape_string($db, $value);
-		$sql .= "${key} = \"${value}\", "; 
+	foreach($save_arr as $key=>$value) {
+		if($value) {
+			$sql .= "${key} = \"" . mysqli_real_escape_string($db, $value) . "\", ";
+		} else {
+			$sql .= "${key} = null, ";
+		}
 	}
 	$sql = trim($sql, ", ");
-		
-	if($rqId == "new") {
+
+	if(!$rqId) {
 		$sql = "INSERT INTO ${rqTable} SET ${sql}";
 	} else {
 		$sql = "UPDATE ${rqTable} SET ${sql} WHERE requisitionId=" . $rqId;
 	}
-	
+
+	//code testing, delete for production
+	$save_activity .= "$sql\n";
+
 	//save rq
-	
+
 	if($_GET["a"]) echo "<span style='font-size: small;'>" . str_replace("<", "&lt;", $sql) . "</span><br />\n";
 	#$db->query($sql) or die("RQ Insert Error: " . $db->error . "<br />\n<code>${sql}</code>\n");
-	if($rqId == "new") { 
+
+	$db->query($sql) or die("RQ_Save_Error: " . $db->error . "<br />\n<code>${sql}</code>\n");
+	if(!$rqId) {
 		$rqId = $db->insert_id;
-		if($_GET["a"]) echo "New rqId: ${rqId}<br />\n"; 
+		if($_GET["a"]) echo "New rqId: ${rqId}<br />\n";
 	}
 
 	//save requisition items
-	
+
 	if($_GET["a"]) echo "<br />RQ Items SQLs:<br />\n";
 
+	$item_number_fields = array("quantity", "price", "extended", "rcvQuantity", "rcvPrice");
+
 	foreach($items as $row) {
-		$row["requisitionId"] = $rqId;
-		$itemId = $row["requisitionItemId"];
-		unset($row["requisitionItemId"]);
-		$fields = array_keys($row);
-		foreach($row as $key=>$value) { $row[$key] = mysqli_real_escape_string($db, $value); }
-		if(!$itemId && $row["del"] != 1) {
-			//items to insert
-			$sql = "INSERT INTO ${rqItemsTable} (" . implode(", ", $fields) . ") VALUES (\"" . implode("\", \"", $row) . "\")";
-		} else if(is_numeric($itemId) && $row["del"] != 1) {
-			//items to update
-			$sql = "UPDATE ${rqItemsTable} SET ";
-			foreach($row as $key=>$value) { $sql .= $key . " = \"" . $value . "\", "; }
-			$sql = trim($sql, ", ") . " WHERE requisitionItemId = ${itemId}";
-		} else if(is_numeric($itemId) && $row["del"] == 1) {
-			//items to delete
-			$sql = "UPDATE ${rqItemsTable} SET is_valid = \"void " . date("Y-m-d H:i") . "\" WHERE requisitionItemId = MID(\"${itemId}\", 2, 20)";
+		$itemId = $row->requisitionItemId;
+		$del = $row->del;
+		unset($row->requisitionItemId, $row->del);
+		$sqlData = " SET ";
+		foreach($row as $key=>$value) {
+			$sqlData .= $key . " = ";
+			if($value) {
+				if(in_array($key, $item_number_fields)) {
+					$sqlData .=  1 * $value . ", ";
+				} else {
+					$sqlData .= "\"" . mysqli_real_escape_string($db, $value) . "\", ";
+				}
+			} else {
+				$sqlData .= "null, ";
+			}
 		}
+		$sqlData = trim($sqlData, ", ");
+
+		if(!$itemId && $del != 1) {
+			//items to insert
+			$sql = "INSERT INTO ${rqItemsTable} " . str_replace("SET ", " SET requisitionId = ${rqId}, ", $sqlData);
+
+			//code testing, delete for production
+			$save_activity .= "$sql\n";
+
+		} else if(is_numeric($itemId) && $del != 1) {
+			//items to update
+			$sql = "UPDATE ${rqItemsTable} " . $sqlData . " WHERE requisitionItemId = ${itemId}";
+
+			//code testing, delete for production
+			$save_activity .= "$sql\n";
+
+		} else if(is_numeric($itemId) && $del == 1) {
+			//items to delete
+			$sql = "UPDATE ${rqItemsTable} SET isValid = \"x" . date("ymdHi") . "\" WHERE requisitionItemId = ${itemId}";
+
+			//code testing, delete for production
+			$save_activity .= "$sql\n";
+
+		}
+
 		if($_GET["a"]) "<span style='font-size: small;'>" . str_replace("<", "&lt;", $sql) . "</span><br /><br /><br />\n";
-		#$db->query($sql) or die("RQ Item Save Error: " . $db->error . "<br />\n<code>${sql}</code>\n");
+
+		$db->query($sql) or die("RQ_Item_Save_Error: " . $db->error . "<br />\n<code>${sql}</code>\n");
 	}
-	
+
 }
 
-if($_GET["requisitionId"] && is_numeric($_GET["requisitionId"])) {
+if($rqId && is_numeric($rqId)) {
 
 	//one rq
-	
+
 	$sql = "
 		SELECT
-			a.*, CONCAT(c.fname, ' ', c.lname) AS preparedByName, 
-			b.vendorName, d.description, 
-			SUM(IF(d.requisitionId, 1, 0)) AS numItems, 
+			a.*, CONCAT(c.fname, ' ', c.lname) AS preparedByName,
+			b.vendorName, d.description,
+			SUM(IF(d.requisitionId, 1, 0)) AS numItems,
 			SUM(ROUND(d.quantity * d.price, 3)) AS rqAmount
 		FROM
-			${rqTable} a LEFT JOIN 
-			${vendorTable} b ON a.vendorId=b.vendorId LEFT JOIN 
-			${staffTable} c ON a.preparedById=c.persid LEFT JOIN 
-			${rqItemsTable} d ON a.requisitionId=d.requisitionId 
+			${rqTable} a LEFT JOIN
+			${vendorTable} b ON a.vendorId=b.vendorId LEFT JOIN
+			${staffTable} c ON a.preparedById=c.persid LEFT JOIN
+			${rqItemsTable} d ON a.requisitionId=d.requisitionId AND d.isValid NOT LIKE 'x%'
 		WHERE
-			a.isValid NOT LIKE 'void%' AND d.isValid NOT LIKE 'void%' AND 
+			a.isValid NOT LIKE 'void%' AND
 			a.requisitionId=" . $_GET["requisitionId"];
 	$rs = $db->query($sql) or die($db->error);
 	$data["rq"] = $rs->fetch_assoc();
 
 	//items for the one rq
 
-	$sql2 = "SELECT * FROM ${rqItemsTable} WHERE requisitionId = " . $_GET["requisitionId"];
+	$sql2 = "SELECT * FROM ${rqItemsTable} WHERE requisitionId = " . $_GET["requisitionId"] . " AND isValid NOT LIKE 'x%'";
 	$rs = $db->query($sql2) or die($db->error);
 	if(mysqli_num_rows($rs)) {
 		while($tmp = $rs->fetch_assoc()) { $data["rq"]["items"][] = $tmp; }
@@ -117,14 +181,14 @@ if($_GET["requisitionId"] && is_numeric($_GET["requisitionId"])) {
 
 	$sql = "
 	SELECT
-		a.*, CONCAT(c.fname, ' ', c.lname) AS preparedByName, 
-		b.vendorName, d.description AS itemDescription, SUM(IF(d.requisitionId, 1, 0)) AS numItems, 
+		a.*, CONCAT(c.fname, ' ', c.lname) AS preparedByName,
+		b.vendorName, d.description AS itemDescription, SUM(IF(d.requisitionId, 1, 0)) AS numItems,
 		SUM(ROUND(d.quantity * d.price, 3)) AS rqAmount
 	FROM
-		${rqTable} a LEFT JOIN 
-		${vendorTable} b ON a.vendorId=b.vendorId LEFT JOIN 
-		${staffTable} c ON a.preparedById=c.persid LEFT JOIN 
-		${rqItemsTable} d ON a.requisitionId=d.requisitionId 
+		${rqTable} a LEFT JOIN
+		${vendorTable} b ON a.vendorId=b.vendorId LEFT JOIN
+		${staffTable} c ON a.preparedById=c.persid LEFT JOIN
+		${rqItemsTable} d ON a.requisitionId=d.requisitionId
 	WHERE
 		a.isValid NOT LIKE 'void%' AND d.isValid NOT LIKE 'void%'
 	GROUP BY a.requisitionId
@@ -140,6 +204,10 @@ if(isset($_GET["a"])) {
 	echo "<span style='font-size: small;'>" . str_replace("<", "&lt;", $sql) . "</span><br />\n";
 	echo "<pre>"; print_r($data); echo "</pre>";
 } else {
+
+	//code testing, delete for production
+	#if($save_activity) $data["save_activity"] = $save_activity;
+
 	$myJSON = json_encode($data);
 	echo $myJSON;
 }
