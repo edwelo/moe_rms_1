ab78fc03b84f6dc5cde220c73749abf6fa6b8825
Administrator <admin@tmpsrvr1.moe>
2018-07-19 19:01:54 +0900

    initial db split attempt

--- api/backend.php
+++ api/backend.php
@@ -7,6 +7,43 @@ date_default_timezone_set("Pacific/Palau");
 
 $page_name = $_GET["pn"];
 
+$html = "
+<h3>Welcome to the MOE Expenditure Tracker App Backend</h3>
+
+<p>
+<span style='color: grey;'>Look at other modules:</span>&nbsp;&nbsp;
+[ <a href=\"?pn=Requisitioning&a=1\">Requisitioning</a> ]
+[ <a href=\"?pn=Receiving&a=1\">Receiving</a> ]
+[ <a href=\"?pn=Distribution&a=1\">Distribution</a> ]
+[ <a href=\"?pn=Inventory&a=1\">Inventory</a> ]
+</p>
+";
+
+if(!$page_name) { echo $html; exit; }
+
+unset($rqId);
+if($_GET["requisitionId"] && ($_GET["requisitionId"] == "new" || is_numeric($_GET["requisitionId"]))) {
+	$rqId = $_GET["requisitionId"];
+	$heading = "${page_name} Form Data, rqId #${rqId}";
+} else {
+	$heading = "${page_name} List Data";
+}
+
+if($_GET["a"]) {
+	echo $html;
+	echo "
+<span style='font-weight: bold;'>${heading}</span>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<span style='font-size: small; color: grey;'> Enter an RqId to look at form data:</span>
+<form style='display: inline; margin: 0px;' action='' method=get>
+	<input type=hidden name=pn value='${page_name}' />
+	<input type=text name=requisitionId value='' size=5 />
+	<input type=hidden name=a value=1 />
+	<input type=submit value='go' />
+</form>
+	";
+}
+
 $db_host = "mysql.moe"; $db_user = "mysql"; $db_pass = "mysql";
 if($_SERVER["REMOTE_ADDRESS"] == "127.0.0.1" || $_SERVER["REMOTE_ADDRESS"] == "::1") {
 	$db_host = "localhost";
@@ -18,13 +55,6 @@ $rqItemsTable = "finance3.requisitions_items";
 $vendorTable = "finance3.vendors";
 $staffTable = "personnel.tblbiodata";
 
-unset($rqId);
-if($_GET["requisitionId"] == "new" || is_numeric($_GET["requisitionId"])) {
-	$rqId = $_GET["requisitionId"]; $type = "Data for Form";
-} else {
-	$type = "Data for List";
-}
-
 if(isset($_GET["save"])) {
 	require_once("includes/${page_name}_save.inc");
 }
@@ -32,14 +62,8 @@ if(isset($_GET["save"])) {
 require_once("includes/${page_name}_read.inc");
 
 if($_GET["a"]) {
-	echo "<p>Page Name: ${page_name}<br />Data Type: ${type}</p>";
-	echo "<p>Read SQLs:<br />" . $sqlHTML . "</p>";
-	
-	echo "<p>Result Array: <pre style='font-size: small;'>"; print_r($data); echo "</pre></p>";
-	$myJSON = json_encode($data);
-	
-	echo "<p>Returned JSON, encoded from above array:</p>";
-	echo "<p style='font-size: small;'>" . json_encode($data) . "</p>";
+	echo "<p>Read SQLs:</p>" . $sqlHTML;
+	echo "<pre>"; print_r($data); echo "</pre>";
 } else {
 
 	//code testing, delete for production
--- templates/frontend_distribution.tpl
+++ templates/frontend_distribution.tpl
@@ -1,28 +1,70 @@
-    <div class = "align-left" style="width: 30%;"">
-      <table id = "distTable">
-        <caption> poNo, requisitionItemId, description, quantity </caption>
-        <tr> 
-          <th> PO# </th>
-          <th> Req Item Id </th>
-          <th> Description </th>
-          <th> Quantity </th>
-          <th> </th>
-        </tr>
-        <tr> 
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td class="btn btn-info btn-lg" id = "plus"> + </td>
-        </tr>
-      </table>
-    </div>
-    <div class = "align-right" style="width: 60%;">
-      <table id = "distTable2">
-        <tr> 
-          <th> Req Item Id </th>
-          <th> Req Id </th>
-          <th> Asset No </th>
-        </tr>
-      </table>
-    </div>
+	<div id="listView">
+
+		List of Distributions.
+		<span style="color: grey;">Click on a row to edit.</span> <br><br>
+
+		<table id="listTable" class="tbl">
+			<caption>
+				distributionId,refPOs,numItems,deliveredBy,deliveredDate,destination,receivedBy,receivedDate,remarks
+			</caption>
+			<tr>
+				<th>Id</th>
+				<th>POs / numItems</th>
+				<th>Dlvr By/Date</th>
+				<th>Destination /<br />Rcv By/Date</th>
+				<th>Remarks</th>
+			</tr>
+			<tr style="vertical-align: top; cursor: pointer;">
+				<td>
+					<span></span>
+				</td>
+				<td>
+					<span></span><br />
+					<span></span>
+				</td>
+				<td>
+					<span></span><br />
+					<span></span>
+				</td>
+				<td>
+					<span></span><br />
+					<span></span><br />
+					<span></span>
+				</td>
+				<td>
+					<span></span>
+				</td>
+			</tr>
+		</table>
+
+	</div>
+	<div id="formView">
+		<div class = "align-left" style="width: 30%;"">
+		  <table id = "distTable">
+			<caption> poNo, requisitionItemId, description, quantity </caption>
+			<tr>
+			  <th> PO# </th>
+			  <th> Req Item Id </th>
+			  <th> Description </th>
+			  <th> Quantity </th>
+			  <th> </th>
+			</tr>
+			<tr>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td></td>
+			  <td class="btn btn-info btn-lg" id = "plus"> + </td>
+			</tr>
+		  </table>
+		</div>
+		<div class = "align-right" style="width: 60%;">
+		  <table id = "distTable2">
+			<tr>
+			  <th> Req Item Id </th>
+			  <th> Req Id </th>
+			  <th> Asset No </th>
+			</tr>
+		  </table>
+		</div>
+	</div>
